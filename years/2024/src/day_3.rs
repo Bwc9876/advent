f4cc148b08b034d1bdd36feaf9c5c97c10c5e963
Ben C <bwc9876@gmail.com>
2024-12-03 00:32:11 -0500

    Day 3

--- years/2024/src/day_3.rs
+++ years/2024/src/day_3.rs
@@ -1,17 +1,31 @@
 
 use advent_core::{Day, day_stuff, ex_for_day};
+use regex::RegexBuilder;
 
 pub struct Day3;
 
+const RE: &str = r"mul\((\d+),(\d+)\)";
+
+fn re_do(i: &str) -> u64 {
+    let re = RegexBuilder::new(RE).multi_line(true).build().unwrap();
+    re.captures_iter(i).map(|c| {
+        c.get(1).unwrap().as_str().parse::<u64>().unwrap() 
+        * 
+        c.get(2).unwrap().as_str().parse::<u64>().unwrap()
+    }).sum::<u64>()
+}
+
 impl Day for Day3 {
 
-    day_stuff!(3, "", "");
+    day_stuff!(3, "161", "48");
 
-    fn part_1(_input: Self::Input) -> Option<String> {
-        None
+    fn part_1(input: Self::Input) -> Option<String> {
+        Some(re_do(&input).to_string())
     }
 
-    fn part_2(_input: Self::Input) -> Option<String> {
-        None
+    fn part_2(input: Self::Input) -> Option<String> {
+        Some(input.split("do()").map(|v| 
+            re_do(v.split("don't()").nth(0).unwrap())
+        ).sum::<u64>().to_string())
     }
 }
